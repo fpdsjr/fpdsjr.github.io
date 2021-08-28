c84dc73c08ad71723c31670742b7da2fbce32c12
Flavio Junior <flaviopds.jr@gmail.com>
2021-08-28 17:26:06 -0300

    adiciona menu e atualiza favicon

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+const hamburguer = document.querySelector(".hamburguer");
+
+hamburguer.addEventListener("click", () => {
+  document.querySelector(".container").classList.toggle("show-menu");
+})
